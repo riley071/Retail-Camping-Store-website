63ce565657eac592fba1a5f4fbc0950ad0990f95
Riley matewere <81653537+riley071@users.noreply.github.com>
2023-03-10 08:44:52 +0100

    Add files via upload

---
+++ Rcc website/js/index.js
@@ -0,0 +1,41 @@
+const hamburer = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list");
+
+if (hamburer) {
+  hamburer.addEventListener("click", () => {
+    navList.classList.toggle("open");
+  });
+}
+
+// Popup
+const popup = document.querySelector(".popup");
+const closePopup = document.querySelector(".popup-close");
+
+if (popup) {
+  closePopup.addEventListener("click", () => {
+    popup.classList.add("hide-popup");
+  });
+
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      popup.classList.remove("hide-popup");
+    }, 1000);
+  });
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 7000); // Change image every 2 seconds
+}
+
+
